80e89420f3e4ad81fc1f1f6f3cad4472e8534403
mzusman <morzusman@gmail.com>
2022-03-07 09:31:12 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      
+
+    </div>
+  </header>
+
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+header {
+  padding: 5vh;
+  line-height: 1.5;
+  max-height: 5vh;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+</style>
