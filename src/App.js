aa5998f19457bc1c9eafba2a4752d9c987674cc8
mzusman <mor.zusmann@gmail.com>
2022-03-08 07:29:44 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MainPage from './components/MainPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends React.Component {
+  render() {
+    return <div>
+        <MainPage></MainPage>
     </div>
-  );
+  };
 }
 
-export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default class Menu extends React.Component {
+    constructor(){
+
+    }
+    render(){
+        return <div>
+            HelloMainPage
+        </div>
+    }
+}
